1c15e015e40484df090c34ac3b6d0931dbe6eb4a
Lava Maamo <lama26830@edu.ucl.dk>
2022-05-05 13:22:00 +0200

    sensemovment

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // URL defineres
 const fakebaseUrl="https://excuser.herokuapp.com/v1/excuse/100" // tredjeparts-API
 const baseUrl="https://restexcuses.azurewebsites.net/api/Excuse" // egen REST-API
+const familyUrl="https://excuser.herokuapp.com/v1/excuse/family/" //Familie API
+const partyUrl="https://excuser.herokuapp.com/v1/excuse/party/" //party API
+const colleageUrl="https://excuser.herokuapp.com/v1/excuse/college" //Collage API
+const workUrl="https://excuser.herokuapp.com/v1/excuse/office" //work API
+
+
 // app oprettes
 const app = Vue.createApp({
     data(){ // appens værdier defineres
         return{
-            excuses:[] // tomt array med plads til alverdens undskyldninger
+            excuses:[], // tomt array med plads til alverdens undskyldninger
+            currentMovement:""
         }
     },
     methods: {
@@ -20,6 +27,30 @@ const app = Vue.createApp({
             }catch(ex){ // exception
                 alert(ex.message) // fejlmeddelelse i tilfælde af at noget gik galt
             }
-        }
+        },
+        async senseMovement(baseUrl){
+            try{
+                const result = await axios.get(baseUrl)
+                this.currentMovement = result.data
+                console.log(this.currentMovement)
+            }
+            catch(ex){
+                alert(ex.message) 
+            }
+        },
+
+        getRandomExcuse(category){
+            this.getRandomExcuseHelper(currentMovement)
+
+        },
+        getRandomExcuseHelper(currentMovement){
+            
+        },
+        detectMovement(){
+
+        },
+        getAllSelfGeneratedExcuses(){
+
+        },
     }
 }).mount("#app") // appen bliver mounted
