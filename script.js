1ab54f635437b4223cf415cafcb764fd7c840c30
DreamMln <maix2374@edu.zealand.dk>
2022-05-05 13:57:18 +0200

    changesHtml

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // URL defineres
 const fakebaseUrl="https://excuser.herokuapp.com/v1/excuse/100" // tredjeparts-API
 const baseUrl="https://restexcuses.azurewebsites.net/api/Excuse" // egen REST-API
+const baseMovementUrl="https://restexcuses.azurewebsites.net/api/Movement" // egen movement REST-API
 const familyUrl="https://excuser.herokuapp.com/v1/excuse/family/" //Familie API
 const partyUrl="https://excuser.herokuapp.com/v1/excuse/party/" //party API
 const colleageUrl="https://excuser.herokuapp.com/v1/excuse/college" //Collage API
@@ -12,7 +13,8 @@ const app = Vue.createApp({
     data(){ // appens værdier defineres
         return{
             excuses:[], // tomt array med plads til alverdens undskyldninger
-            currentMovement:""
+            currentMovement:"",
+            currentCategory:"",
         }
     },
     methods: {
@@ -28,9 +30,9 @@ const app = Vue.createApp({
                 alert(ex.message) // fejlmeddelelse i tilfælde af at noget gik galt
             }
         },
-        async senseMovement(baseUrl){
+        async senseMovement(){
             try{
-                const result = await axios.get(baseUrl)
+                const result = await axios.get(baseMovementUrl)
                 this.currentMovement = result.data
                 console.log(this.currentMovement)
             }
@@ -43,8 +45,14 @@ const app = Vue.createApp({
             this.getRandomExcuseHelper(currentMovement)
 
         },
-        getRandomExcuseHelper(currentMovement){
-            
+        getRandomExcuseHelper(){
+            //oversætter bevæg. til kategori
+            this.senseMovement()
+            if (this.currentMovement == "right")
+            {
+                this.getAllSelfGeneratedExcusesHelper(familyUrl)
+            }
+
         },
         detectMovement(){
 
